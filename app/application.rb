728213771368fbdc99a1f89f383231bd5a458fc8
Daniel Sullivan <dansullivan19@gmail.com>
2019-07-16 16:52:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,8 +8,11 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      
-    
+      item = req.params["item"]
+      @@items.find (|e| e == item)
+    else 
+      resp.write ""
+      .status = 404
     
   
   end
