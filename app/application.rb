a2407ea3eadb3d533c59fcc5d27482d972446cd2
Daniel Sullivan <dansullivan19@gmail.com>
2019-07-16 17:04:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,6 +12,11 @@ class Application
     if req.path.match(/items/)
       item = req.path.split("/items/").last
       # @@items.find {|e| e.name == item}
+      @@items.collect do |e| 
+        e.name == item 
+        e.price
+        binding.pry
+      end
     else 
       resp.write "Route not found"
       resp.status = 404
